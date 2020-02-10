1f7f520cd17cc6a5cec6ed29c8c7b937a6270236
Dane Burns <dane@lizaburnsandassociates.com>
2020-02-09 17:43:27 -0700

    works but not DRY

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ var h1 = document.createElement('h1')
 var p = document.createElement('p')
 var btn = document.createElement('button')
 var start = document.querySelector('#start')
-var quizCount = 0;
+var timeEl = document.getElementById('timerCount')
+var timer = parseInt(timeEl.textContent)
+console.log(timeEl)
 
 idArray = [];
 questionBody = ['Commonly used data types DO NOT include:', 'The condition in an if/else statement is enclosed within___.', 'Arrays in JavaScript can be used to store___.', 'String values must be enclosed within__ when being assigned to variables.', 'A very useful tool used during development and debugging for printing content to the debugger is:']
@@ -13,11 +15,43 @@ questionsArray2 = ['quotes', 'curly brackets', 'parentheses', 'square brackets']
 questionsArray3 = ['numbers and strings', 'other arrays', 'booleans', 'all of the above']
 questionsArray4 = ['commas', 'curly brackets', 'quotes', 'parentheses']
 questionsArray5 = ['javascript', 'terminal/bash', 'for loops', 'console.log']
-correctAnswers = ['strings', 'parentheses', 'all of the above', 'quotes','console.log' ]
 questionsArrayMaster = [questionsArray1, questionsArray2, questionsArray3, questionsArray4, questionsArray5]
-ticker = 1;
+ticker = 0;
+correctAnswers = ['strings', 'parentheses']
 
-console.log(questionBody[0])
+
+function setTime() {
+    var timer = 100
+    timerInterval = setInterval(function () {
+        timer--
+        timeEl.textContent = timer
+
+        if (timer === 0) {
+            clearInterval(timerInterval);
+            alert('You lose!')
+        }
+    }, 200)
+}
+
+function stopTime() {
+    clearInterval(timerInterval)
+}
+
+function setTimeCorrect() {
+
+    timerInterval = setInterval(function () {
+        timer--
+        console.log(timer)
+        timeEl.textContent = timer
+
+        if (timer === 0) {
+            clearInterval(timerInterval);
+            alert('You lose!')
+        }
+    }, 200)
+
+}
+
 
 function clearContainer() {
 
@@ -28,6 +62,7 @@ function clearContainer() {
         bodyContainer.removeChild(child);
         child = bodyContainer.lastElementChild;
     }
+    populateQuestion(bodyContainer, questionsArray1)
 
 }
 
@@ -35,7 +70,7 @@ function clearContainer2(questionsArray) {
 
     idArray.length = 0
     questionBody.shift();
-    console.log(questionBody)
+
 
     var child = bodyContainer.lastElementChild;
     while (child) {
@@ -46,11 +81,9 @@ function clearContainer2(questionsArray) {
 }
 
 start.addEventListener('click', function (e) {
-
+    event.preventDefault()
     clearContainer()
-    populateQuestion(bodyContainer, questionsArray1)
-
-
+    setTime()
 })
 
 function populateQuestion(parent, questionArray) {
@@ -73,8 +106,8 @@ function populateQuestion(parent, questionArray) {
         one.classList.add('mt-3')
     })
 
-    
-    
+
+
 
     questionArray.forEach(element2 => {
         var buttons = document.getElementById(questionArray.indexOf(element2))
@@ -82,70 +115,96 @@ function populateQuestion(parent, questionArray) {
 
 
     });
-    
-    if(questionArray.includes('quotes') && questionArray === questionsArray4){
-        var buttons = document.getElementById('2')
-        console.log(buttons)
-            buttons.classList.add('correct')
-        }
-}
 
+    if (questionArray.includes('quotes') && questionArray === questionsArray4) {
+        var buttons = document.getElementById('2')
+
+        buttons.classList.add('correct')
+    }
+}
 
 bodyContainer.addEventListener('click', function (e) {
     event.preventDefault()
-    if (event.target.innerText === 'strings') {
+    for (let i = 0; i < questionsArrayMaster.length; i++) {
+        if (correctAnswers.includes(event.target.innerText)){
+            ticker++
+            stopTime()
+            timer+= 50
+            setTimeCorrect();
+            clearContainer2(questionsArrayMaster[i])
+         }
 
-    ticker++
-    clearContainer2(questionsArrayMaster[ticker]) }
+    }
 
-if (event.target.innerText === 'parentheses') {
-    
-    ticker++
-    clearContainer2(questionsArrayMaster[ticker])
+})
 
-
-}
-if (event.target.innerText === 'all of the above') {
-
-    ticker++
-    clearContainer2(questionsArrayMaster[ticker])
-
-
-}
-if (event.target.classList.contains('correct')) {
-
-    ticker++
-    clearContainer2(questionsArrayMaster[ticker])
-
-
-} 
-
-if (event.target.innerText === 'console.log') {
-
-    ticker++
-    clearContainer2(questionsArrayMaster[ticker])
-
-
-} 
-
-
-else if (event.target.innerText === 'booleans' || event.target.innerText === 'alerts' || event.target.innerText === 'numbers'
-    || event.target.innerText === 'curly brackets' || event.target.innerText === 'square brackets') {
-    ticker++
-    clearContainer2(questionsArrayMaster[ticker])
-    // var redText = bodyContainer.appendChild(makeRow)
-    // redText.setAttribute('class', 'redText')
-    // redText.classList.add('p-5')
-    // bodyContainer.lastChild.innerText = 'WRONG!'
-
-}
-    })
+//     if (event.target.innerText === 'strings') {   
+//     ticker++
+//     stopTime()
+//     timer += 50
+//     setTimeCorrect();
+//     clearContainer2(questionsArrayMaster[ticker]) 
 
 
 
+// }
+
+// if (event.target.innerText === 'parentheses') {
+
+//     ticker++
+//     stopTime()
+//     timer += 20
+//     setTimeCorrect();
+
+//     clearContainer2(questionsArrayMaster[ticker])
 
 
-console.log(quizCount)
+// }
+// if (event.target.innerText === 'all of the above') {
+
+//     ticker++
+//     stopTime()
+//     timer += 20
+//     setTimeCorrect();
+//     clearContainer2(questionsArrayMaster[ticker])
+
+
+// }
+// if (event.target.classList.contains('correct')) {
+
+//     ticker++
+//     stopTime()
+//     timer += 20
+//     setTimeCorrect();
+//     clearContainer2(questionsArrayMaster[ticker])
+
+
+// } 
+
+// if (event.target.innerText === 'console.log') {
+
+//     ticker++
+//     stopTime()
+//     timer += 20
+//     setTimeCorrect();
+//     clearContainer2(questionsArrayMaster[ticker])
+
+
+// } 
+
+// else  {
+//     ticker++
+//     clearContainer2(questionsArrayMaster[ticker])
+//     // var redText = bodyContainer.appendChild(makeRow)
+//     // redText.setAttribute('class', 'redText')
+//     // redText.classList.add('p-5')
+//     // bodyContainer.lastChild.innerText = 'WRONG!'
+
+// }
+
+
+
+
 
 
 
